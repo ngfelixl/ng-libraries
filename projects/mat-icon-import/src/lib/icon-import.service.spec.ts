566aa2445b33b36acff5c9c184363fd89a79718c
Felix Lemke <flemke.dev@gmail.com>
2018-09-26 13:28:43 +0200

    Target github project homepage

--- projects/mat-icon-import/src/lib/icon-import.service.spec.ts
+++ projects/mat-icon-import/src/lib/icon-import.service.spec.ts
@@ -2,14 +2,18 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { IconImportService } from './icon-import.service';
 
-describe('MatIconImportService', () => {
+describe('IconImportService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [IconImportService]
+      providers: [
+        IconImportService,
+        { provide: 'icons', useValue: { key: 'account_circle', path: './assets/mat-icons' }}
+      ]
     });
   });
 
   it('should be created', inject([IconImportService], (service: IconImportService) => {
     expect(service).toBeTruthy();
   }));
+
 });
