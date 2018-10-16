5bb3e1c04be707eb9b195aad557930e6b715ef98
Felix Lemke <flemke.dev@gmail.com>
2018-10-16 12:32:44 +0200

    Implemented responsive histograms

--- projects/ng-d3plot/src/lib/histogram.component.ts
+++ projects/ng-d3plot/src/lib/histogram.component.ts
@@ -26,6 +26,9 @@ export class HistogramComponent implements AfterViewInit {
   private margin = ({top: 30, right: 10, bottom: 35, left: 40});
   private x: ScaleLinear<number, number>;
   private y: ScaleLinear<number, number>;
+  private xAxis: (g: any) => any;
+  private yAxis: (g: any) => any;
+  private bins: Bin<number, number>[];
 
   @HostListener('window:resize', [])
   onresize() {
@@ -50,39 +53,41 @@ export class HistogramComponent implements AfterViewInit {
       .domain(extent(this.data)).nice()
       .range([this.margin.left, this.width - this.margin.right]);
 
-    const bins = histogram()
+    this.bins = histogram()
       .domain((<any>this.x).domain())
       .thresholds(this.x.ticks(10))(this.data);
 
     this.y = scaleLinear()
-      .domain([0, +d3Max(bins, (d: any[]) => d.length)]).nice()
+      .domain([0, +d3Max(this.bins, (d: any[]) => d.length)]).nice()
       .range([this.height - this.margin.bottom, this.margin.top]);
 
-    const xAxis = g => g
+    this.xAxis = g => g
       .attr('transform', `translate(0,${this.height - this.margin.bottom})`)
+      .attr('class', 'x-axis')
       .call(axisBottom(this.x).tickSizeOuter(0))
       .call(g00 => g.append('text')
-          .attr('x', this.width - this.margin.right)
-          .attr('y', -4)
-          .attr('fill', '#000')
-          .attr('font-weight', 'bold')
-          .attr('text-anchor', 'end')
-          .text(this.config.xLabel));
+        .attr('x', this.width - this.margin.right)
+        .attr('y', -4)
+        .attr('fill', '#000')
+        .attr('font-weight', 'bold')
+        .attr('text-anchor', 'end')
+        .text(this.config.xLabel));
 
-    const yAxis = g => g
+    this.yAxis = g => g
       .attr('transform', `translate(${this.margin.left},0)`)
+      .attr('class', 'y-axis')
       .call(axisLeft(this.y))
       .call(g0 => g0.select('.domain').remove())
       .call(g0 => g0.select('.tick:last-of-type text').clone()
-          .attr('x', 4)
-          .attr('text-anchor', 'start')
-          .attr('font-weight', 'bold')
-          .text(this.config.yLabel));
+        .attr('x', 4)
+        .attr('text-anchor', 'start')
+        .attr('font-weight', 'bold')
+        .text(this.config.yLabel));
 
     const bar = this.svg.append('g')
       .attr('fill', 'steelblue')
       .selectAll('rect')
-      .data(bins)
+      .data(this.bins)
       .enter().append('rect')
       .attr('x', (d: any) => this.x(d.x0) + 1)
       .attr('width', (d: any) => Math.max(0, this.x(d.x1) - this.x(d.x0) - 1))
@@ -90,26 +95,39 @@ export class HistogramComponent implements AfterViewInit {
       .attr('height', (d: any) => this.y(0) - this.y(d.length));
 
     this.svg.append('g')
-      .call(xAxis);
+      .call(this.xAxis);
 
     this.svg.append('g')
-      .call(yAxis);
+      .call(this.yAxis);
 
     this.resize$.pipe(
       debounceTime(200)
     ).subscribe(() => {
-      // this.scale();
-      // this.draw();
+      this.scale();
     });
   }
 
-  /* scale() {
+  scale() {
     this.width = this.element.nativeElement.clientWidth;
     this.height = Math.round(this.width / 4 * 3);
 
     this.svg.attr('viewBox', `0 0 ${this.width} ${this.height}`);
-  }
 
-  draw() {
-  } */
+    this.x.range([this.margin.left, this.width - this.margin.right]);
+    this.y.range([this.height - this.margin.bottom, this.margin.top]);
+
+    this.svg.selectAll('rect')
+      .attr('x', (d: any) => this.x(d.x0) + 1)
+      .attr('width', (d: any) => Math.max(0, this.x(d.x1) - this.x(d.x0) - 1))
+      .attr('y', (d: any) => this.y(d.length))
+      .attr('height', (d: any) => this.y(0) - this.y(d.length));
+
+    this.svg.selectAll('.y-axis')
+      .attr('transform', `translate(${this.margin.left},0)`)
+      .call(axisLeft(this.y));
+
+    this.svg.selectAll('.x-axis')
+      .attr('transform', `translate(0,${this.height - this.margin.bottom})`)
+      .call(axisBottom(this.x).tickSizeOuter(0));
+  }
 }
