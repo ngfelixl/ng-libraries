ac7e02e9994b5b35e7081d6aabb298229092dca4
Felix Lemke <flemke.dev@gmail.com>
2018-10-17 10:05:28 +0200

    Increased right margin

--- projects/ng-d3plot/src/lib/base-class.ts
+++ projects/ng-d3plot/src/lib/base-class.ts
@@ -4,7 +4,7 @@ import { Subject } from 'rxjs';
 import { Selection, BaseType } from 'd3';
 
 export class BaseClass {
-  public margin = ({top: 30, right: 10, bottom: 35, left: 40});
+  public margin = ({top: 30, right: 20, bottom: 35, left: 40});
   public height: number;
   public width: number;
   public svg: Selection<BaseType, {}, HTMLElement, any>;
